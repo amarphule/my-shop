6693d92f85cc9ae3534b12e7536473f09087b06b
Amar Phule <amardeepphule@gmail.com>
2024-06-14 00:46:27 +0530

    customer table with dummy data

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import CustomerList from "./components/CustomerList";
+
+export const customers = [
+  {
+    id: 1,
+    name: "John R",
+    noOfPurchase: 10,
+    amount: 1200,
+  },
+  {
+    id: 2,
+    name: "Rocky B",
+    noOfPurchase: 20,
+    amount: 2400,
+  },
+  {
+    id: 3,
+    name: "Burley Rox",
+    noOfPurchase: 8,
+    amount: 960,
+  },
+];
+
 const App = () => {
   return (
-    <div className="text-4xl font-semibold underline text-center py-5">
-      Today's Purchase
-    </div>
+    <>
+      <h1 className="text-4xl font-semibold underline text-center py-5">
+        Today's Purchase
+      </h1>
+      <CustomerList />
+    </>
   );
 };
 
